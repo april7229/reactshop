11b99310b889e1d3937de1e98662a5200eb7298c
april <martinezapril56@yahoo.com>
2019-01-24 19:26:59 -0500

    components

---
+++ store/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h3>hello from Navbar</h3>
+      </div>
+      
+    );
+  }
+}
+
+export default App;
