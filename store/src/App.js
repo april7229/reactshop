497a5ba08d0d34172a03f218c451bfa7dd4f1aa8
april <martinezapril56@yahoo.com>
2019-01-24 21:03:36 -0500

    creating routes

--- store/src/App.js
+++ store/src/App.js
@@ -1,15 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Switch,Route } from 'react-router-dom';
+
+import ProductList from './components/ProductList';
+import Cart from './components/Cart';
+import Details from './components/Details';
+import Navbar from './components/Navbar';
+import Default from './components/Default';
+
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <h3>hello from Navbar</h3>
-      </div>
-      
+      <React.Fragment> 
+        <Navbar />
+        <Switch>
+          <Route exact path='/' component={ProductList} />
+          <Route path='/details' component={Details} />
+          <Route path='/cart' component={Cart} />
+          <Route  component={Default} />
+        </Switch>
+        </React.Fragment>
+       
+     
     );
   }
 }
--- store/src/components/ProductList.js
+++ store/src/components/ProductList.js
@@ -5,7 +5,7 @@ export default class ProductList extends Component {
   render() {
     return (
       <div>
-        <Product></Product>
+        <Product>Hello from produckList </Product>
       </div>
     )
   }
