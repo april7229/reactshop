2ce29d5314b870593d2abd027ce976b5f6d14c75
april <martinezapril56@yahoo.com>
2020-08-07 23:36:37 -0400

    mirrorw

--- store/src/data.js
+++ store/src/data.js
@@ -3,12 +3,12 @@
 export const storeProducts = [
          {
            id: 1,
-           title: "necklace - Black",
+           title: "Mirror -Resin Dried flowers ",
            img: "img/pic1.jpg",
-           price: 0,
+           price: 5,
            company: "",
            info:
-             "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+             "This Hand crafted Mirror is made with dried flowers emersed in high quality resin.",
            inCart: false,
            count: 0,
            total: 0,
